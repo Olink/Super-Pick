14eb4081c30cdab6ec8e477a900397b26456f6e1
Zach Piispanen <pikakillers@hotmail.com>
2011-04-18 19:11:55 -0400

    now with working permissions 'SuperPick.super'

--- src/com/shankshock/SuperPick/SuperPickBlockListener.java
+++ src/com/shankshock/SuperPick/SuperPickBlockListener.java
@@ -2,10 +2,8 @@ package com.shankshock.SuperPick;
 
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockDamageEvent;
 import org.bukkit.event.block.BlockListener;
-import org.bukkit.plugin.Plugin;
 
 import com.shankshock.SuperPick.SuperPick;
 
@@ -21,9 +19,7 @@ public class SuperPickBlockListener extends BlockListener {
     	Player p = event.getPlayer();
     	Material h = p.getItemInHand().getType();
     	Material b = event.getBlock().getType();
-    	if( Plugin.Permissions.Security( p, "SuperPick.super") &&
-    			plugin.users.containsKey( p ) && plugin.users.get( p )
-    			&& b != Material.BEDROCK
+    	if( plugin.users.containsKey( p ) && plugin.users.get( p ) && b != Material.BEDROCK
     			&& (h == Material.DIAMOND_PICKAXE || h == Material.GOLD_PICKAXE ||
     					h == Material.IRON_PICKAXE || h==Material.STONE_PICKAXE ||
     					h == Material.WOOD_PICKAXE ) )
