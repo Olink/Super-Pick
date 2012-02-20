f1ac7de84495d9fa4d99343f7f2f5e91a8888eda
Lucas Nicodemus <nicatron21@gmail.com>
2012-02-20 12:23:08 -0700

    Updated to the new Bukkit event system

--- src/com/shankshock/SuperPick/SuperPickPlayerListener.java
+++ src/com/shankshock/SuperPick/SuperPickPlayerListener.java
@@ -1,18 +1,19 @@
 package com.shankshock.SuperPick;
 
 import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerListener;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
 
-import com.shankshock.SuperPick.SuperPick;
-
-public class SuperPickPlayerListener extends PlayerListener {
+public class SuperPickPlayerListener implements Listener {
     private final SuperPick plugin;
     
     public SuperPickPlayerListener(SuperPick instance) {
         plugin = instance;
     }
     
+    @EventHandler(priority = EventPriority.MONITOR)
     public void onPlayerQuit( PlayerQuitEvent event )
     {
     	Player p = event.getPlayer();
